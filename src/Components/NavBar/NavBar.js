0d05fae78e7fd6d149bbcb5ba6f6ba6b1d067430
Fernando Casais <fpcasais@gmail.com>
2023-07-01 18:16:21 -0300

    formatting code

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,14 +1,14 @@
 import "./NavBar.css"
 import Brand from "./Assets/logo.svg"
-import Boton from "./Assets/Boton.png"
+
 
 
 const NavBar = () => {
     return (
         <section className="navBar">
             <img className="navBar_logo" src={Brand} alt="Logo" />
-            <button className="navBar_button"><img className="navBar_logo" src={Boton}></img></button>
-            
+            <button className="navBar_button" >Contratar</button>
+         
             </section>
     )
 }
